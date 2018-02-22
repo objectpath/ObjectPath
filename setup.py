f496b34e8dc70939612b783ebeddc6e2a5791525
Chris Lapa <chris@lapa.com.au>
2018-02-22 13:35:18 +1100

    convert tabs to spaces.
    Signed-off-by: Chris Lapa <36723261+chris-lapa@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -12,29 +12,29 @@ long_description = (
     )
 
 setup(name='objectpath',
-			version=read('VER').strip(),
-			description='The agile query language for semi-structured data. #JSON',
-			long_description=long_description,
-			url='http://adriank.github.io/ObjectPath',
-			author='Adrian Kalbarczyk',
-			author_email='adrian.kalbarczyk@gmail.com',
-			license='MIT',
-			packages=['objectpath','objectpath.utils','objectpath.core'],
-			# package_dir={'': 'objectpath'},
-			keywords="query, tree, JSON, nested structures",
-			classifiers=[
-				"Development Status :: 6 - Mature",
-				"Intended Audience :: Developers",
-				"Intended Audience :: Science/Research",
-				"License :: OSI Approved :: MIT",
-				"Programming Language :: Python",
-				"Topic :: Software Development :: Libraries :: Python Modules"
-			],
-			zip_safe=True,
-			entry_points = {
-				'console_scripts': [
-					'objectpath = objectpath.shell:main'
-				]
-			},
-			test_suite="tests"
-		)
+            version=read('VER').strip(),
+            description='The agile query language for semi-structured data. #JSON',
+            long_description=long_description,
+            url='http://adriank.github.io/ObjectPath',
+            author='Adrian Kalbarczyk',
+            author_email='adrian.kalbarczyk@gmail.com',
+            license='MIT',
+            packages=['objectpath','objectpath.utils','objectpath.core'],
+            # package_dir={'': 'objectpath'},
+            keywords="query, tree, JSON, nested structures",
+            classifiers=[
+                "Development Status :: 6 - Mature",
+                "Intended Audience :: Developers",
+                "Intended Audience :: Science/Research",
+                "License :: OSI Approved :: MIT",
+                "Programming Language :: Python",
+                "Topic :: Software Development :: Libraries :: Python Modules"
+            ],
+            zip_safe=True,
+            entry_points = {
+                'console_scripts': [
+                    'objectpath = objectpath.shell:main'
+                ]
+            },
+            test_suite="tests"
+        )
